44ecefafdb94571ea562c50d8c14320d802ba646
Daniel J Brieck Jr <djbrieck@gmail.com>
2023-10-05 05:18:23 -0700

    Adding in mock bookmarks file

---
+++ src/app/mock-bookmarks.ts
@@ -0,0 +1,17 @@
+import { Bookmark } from "./bookmark";
+
+export const Bookmarks : Bookmark[]= [
+    { name: "Keybr", url : "https://www.keybr.com/profile", clickCount :24 },
+    { name:"Proton Mail", url:"https://mail.proton.me", clickCount:  23},
+    { name:"Google Voice", url:"https://voice.google.com", clickCount:  22},
+    { name:"eBay", url:"https://www.ebay.com", clickCount:  21},
+    { name:"PodFriend", url:"https://web.podfriend.com", clickCount:  20},
+    { name:"Brieck's Pro", url:"https://brieckspro.com", clickCount:  19},
+    { name:"Udemy", url:"https://www.udemy.com/home/my-courses/learning", clickCount:  18},
+    { name:"Plex", url:"https://app.plex.tv", clickCount:  16},
+    { name:"Epoch Times", url:"https://www.theepochtimes.com", clickCount:  30},
+    { name:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dapibus id turpis eget auctor. Vestibulum nisi mi, aliquet quis porta at, fermentum non velit. Curabitur a pretium dolor, a ornare lorem. Integer tempor convallis ex, sed commodo felis rhoncus vel. Sed sed urna ac sapien ornare feugiat at ut sem. Pellentesque condimentum tempus est, eget placerat est gravida id. Duis odio risus, consequat non egestas non, iaculis quis lorem. In viverra sem aliquet nisl pretium, vel posuere diam tempus. Nunc tristique interdum eros sodales ullamcorper. Vestibulum vestibulum sed nunc sit amet condimentum. Proin malesuada urna in viverra laoreet. Duis vel purus in leo volutpat cursus in cursus ligula. Donec vestibulum diam quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit.", url:"#", clickCount:  3},
+    { name:"ABCDEFGHIJKLMNOPQRSTUVWXYZ", url:"#", clickCount:  2},
+    { name:"The quick brown fox jumps over the lazy dog.", url:"#", clickCount:  1}
+];
+
